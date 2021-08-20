c9635315e8a413c8c82085c5d7291bcd75aa5651
user <supermegavalera9000@gmail.com>
2021-08-20 17:21:19 +0300

    update

--- api/index.js
+++ api/index.js
@@ -2,17 +2,55 @@ const express = require('express')
 const app = express()
 
 app.get('/test', (req, res) => {
-  res.json({
-    success: true
-  })
+	res.json({
+		success: true
+	})
 })
 
+const axios = require('axios')
+const iconv = require('iconv-lite')
+
+const TELEGRAM_API_KEY = require('./config.json').TELEGRAM_API_KEY
+
+const getJoke = async type => {
+	const response = await axios.get(`http://rzhunemogu.ru/RandJSON.aspx?CType=${type}`, {
+		responseType: 'arraybuffer',
+		responseEncoding: 'binary'
+	})
+	const content = iconv.decode(Buffer.from(response.data), 'windows-1251').slice(12, -2)
+	return content
+}
+
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(TELEGRAM_API_KEY, {
+	polling: true
+})
+
+bot.setMyCommands([
+	{
+		command: '/start',
+		description: 'Стартовая команда'
+	},
+	{
+		command: '/anek',
+		description: 'Получить анекдот'
+	}
+])
+
+bot.onText(/\/start/, async message => {
+	await bot.sendMessage(message.chat.id, 'Напиши /anek чтобы получить анекдот')
+})
+
+bot.onText(/\/anek/, async message => {
+	await bot.sendMessage(message.chat.id, await getJoke(1))
+})
 
 module.exports = app
 
 if (require.main === module) {
-  const port = process.env.PORT || 3001
-  app.listen(port, () => {
-    console.log(`API server listening on port ${port}`)
-  })
+	const port = process.env.PORT || 3001
+	app.listen(port, () => {
+		console.log(`API server listening on port ${port}`)
+	})
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class='container'>
+	<div class='container d-flex justify-content-center'>
 		<div>
 			<Logo />
 			<div class='mt-5'>
@@ -11,7 +11,7 @@
 
 <script>
 export default {
-	async asyncData ({ $api }) {
+	async asyncData({ $api }) {
 		const test = await $api('test')
 		return {
 			test
@@ -20,43 +20,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-
-.title {
-	font-family:
-		'Quicksand',
-		'Source Sans Pro',
-		-apple-system,
-		BlinkMacSystemFont,
-		'Segoe UI',
-		Roboto,
-		'Helvetica Neue',
-		Arial,
-		sans-serif;
-	display: block;
-	font-weight: 300;
-	font-size: 100px;
-	color: #35495e;
-	letter-spacing: 1px;
-}
-
-.subtitle {
-	font-weight: 300;
-	font-size: 42px;
-	color: #526488;
-	word-spacing: 5px;
-	padding-bottom: 15px;
-}
-
-.links {
-	padding-top: 15px;
-}
-</style>
+<style scoped></style>
