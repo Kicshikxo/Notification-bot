ef54d932f4492ea24493003d0fbdcce66c9bc412
user <supermegavalera9000@gmail.com>
2021-08-21 14:35:33 +0300

    update

--- api/index.js
+++ api/index.js
@@ -35,15 +35,11 @@ app.post('/dispatch', async (req, res) => {
 })
 
 app.get('/users', async (req, res) => {
-	return res.json({
-		users: await telegramDB.getUsers()
-	})
+	return res.json({ users: await telegramDB.getUsers() })
 })
 
 app.get('/channels', async (req, res) => {
-	return res.json({
-		channels: await discordDB.getChannels()
-	})
+	return res.json({ channels: await discordDB.getChannels() })
 })
 
 telegramBot.onText(/\/start/, async message => {
