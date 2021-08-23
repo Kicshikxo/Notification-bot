e6e442c66e3364dafa266eadf092758d0910301f
user <supermegavalera9000@gmail.com>
2021-08-23 16:37:54 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const telegramDB = require('./telegramDbController')
 const discordDB = require('./discordDbController')
 const telegramBot = require('./telegramBot')
 const discordBot = require('./discordBot')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
