7bc69d45762cb95bac84351aa8358f6aef9c40a2
user <supermegavalera9000@gmail.com>
2021-08-24 12:26:53 +0300

    update

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ app.get('/telegram/users', async (req, res) => {
 		for (const user of await telegramDB.getUsers()) {
 			const userInfo = await telegramBot.getChatMember(user.chatId, user.id)
 			const userAvatarList = await telegramBot.getUserProfilePhotos(user.id)
-			const userAvatarLink = await telegramBot.getFileLink(userAvatarList.photos[0][0].file_id)
+			const userAvatarLink = await telegramBot.getFileLink((userAvatarList.photos || [])[0][0].file_id)
 			users.push({
 				id: userInfo.user.id,
 				firstName: userInfo.user.first_name,
